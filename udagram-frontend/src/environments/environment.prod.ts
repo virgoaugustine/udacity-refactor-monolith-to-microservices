fd7b4f034bb79d4cf70a3b0b2e388bda9e97b19e
virgoaugustine <virgoaugustine@gmail.com>
2021-09-03 10:15:13 +0000

    increase pod loads

---
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appName: 'Udagram',
+  apiHost: 'http://a56e3a5f72f14441c92f8a70ed68c0bc-826246622.us-east-1.elb.amazonaws.com:8080/api/v0'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
